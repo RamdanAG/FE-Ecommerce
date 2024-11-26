84d86b171848f62b200dff76f2de90396125c68b
RamdanAG <ainmeeid@gmail.com>
2024-11-26 05:59:47 -0800

    added Navbar responsive

--- src/componets/elements/Main.jsx
+++ src/componets/elements/Main.jsx
@@ -1,38 +1,14 @@
+import React, { useState, useEffect } from 'react';
 import '../../css/HomePage/Main.css';
 
+
 const Main = () => {
-  return(
+
+  return (
     <>
-    <div class="container">
-        <div class="box box1">\
-            <div className="box-content-1">
-                <div className="box-content-text-1">
-                    Lorem ipsum, dolor sit amet consectetur <br/>adipisicing elit. Officiis, voluptate voluptatum. Fugiat sapiente
-                </div>
-                <div className="box-content-button-1">
-                    <button>Check Out Now</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="box box2">
-            <div className="box-content-2">
-                <div className="box-content-text-2">
-                Baju - Dari Berbagai Kategori
-                </div>
-            </div>
-        </div>
-
-        <div class="box box3">
-            <div className="box-content-3">
-                <div className="box-content-text-3">
-                    Baju - Dari Berbagai Kategori
-                </div>
-            </div>
-        </div>
-  </div>
+    <div className="container"></div>
     </>
-  )
+  );
 };
 
 export default Main;
--- src/componets/elements/NavBar.jsx
+++ src/componets/elements/NavBar.jsx
@@ -1,37 +1,83 @@
-import '../../css/Navbar.css';
-import '../../css/index.css';
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart, faUser, faLanguage, faBars } from "@fortawesome/free-solid-svg-icons";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart, faUser, faLanguage } from '@fortawesome/free-solid-svg-icons'; // Import ikon yang digunakan
-
-import Logo from '../../../public/asset/image/Logo.png';
+import Logo from "../../../public/asset/image/Logo.png";
+import "../../css/Navbar.css";
+import "../../css/index.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [languageOpen, setLanguageOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const toggleLanguageDropdown = () => {
+    setLanguageOpen(!languageOpen);
+  };
+
+  const MenuLanguage = ["Indonesia", "English", "Japan"];
+
   return (
-    <>
-      <nav>
-        <img src={Logo} alt="Logo" />
-        <div className="Listbar-nav">
-          <ul>
+    <nav>
+      <img src={Logo} alt="Logo" />
+
+      <div className="Listbar-nav-dekstop">
+        <ul>
+          <li>Women</li>
+          <li>Men</li>
+          <li>Kids</li>
+          <li>Baby</li>
+        </ul>
+      </div>
+
+      <div className="Listbar-nav">
+        <ul className={menuOpen ? "active" : ""}>
+          <div className="Lisbar-nav-hr">
             <li>Women</li>
+            <hr />
+          </div>
+          <div className="Lisbar-nav-hr">
             <li>Men</li>
+            <hr />
+          </div>
+          <div className="Lisbar-nav-hr">
             <li>Kids</li>
+            <hr />
+          </div>
+          <div className="Lisbar-nav-hr">
             <li>Baby</li>
-          </ul>
-        </div>
-        <div className="navbar-icons">
-          <button className="icon-btn">
-            <FontAwesomeIcon icon={faShoppingCart} />
-          </button>
-          <button className="icon-btn">
-            <FontAwesomeIcon icon={faUser} />
-          </button>
-          <button className="icon-btn">
+            <hr />
+          </div>
+        </ul>
+      </div>
+
+      <div className="navbar-icons">
+        <button className="icon-btn menu-bars" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <button className="icon-btn menu-normal">
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </button>
+        <button className="icon-btn menu-normal">
+          <FontAwesomeIcon icon={faUser} />
+        </button>
+        <div className="language-dropdown">
+          <button className="icon-btn menu-normal" onClick={toggleLanguageDropdown}>
             <FontAwesomeIcon icon={faLanguage} />
           </button>
+          {languageOpen && (
+            <ul className="dropdown-menu">
+              {MenuLanguage.map((menu) => (
+                <li key={menu}>{menu}</li>
+              ))}
+            </ul>
+          )}
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
