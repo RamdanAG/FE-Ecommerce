9bf9309bec35dff594f4434438a8ddb109fe92c3
RamdanAG <ainmeeid@gmail.com>
2024-11-28 18:32:36 -0800

    add section, fixed navbar, remove some files,not added router to other pages

--- src/componets/elements/Main.jsx
+++ src/componets/elements/Main.jsx
@@ -1,12 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../../css/HomePage/Main.css';
+import "../../css/index.css";
+import { Link } from 'react-router-dom';
 
+import Slide4 from "../../../public/asset/image/Slider/Slide4.webp";
 
 const Main = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <>
-    <div className="container"></div>
+    <div className='section-header'>
+      <div className='section-header-content'>
+          <img src={Slide4} alt="" />
+          <div className="section-header-content-text">
+            <ul>
+              <li><a href="">Kaos</a></li>
+              <li><a href="">Unisex</a></li>
+              <li><a href="">Kolaborasi</a></li>
+            </ul>
+            <a href="/Women">
+            <h1>DAPATKAN KAOS TERBARU JUJUTSU KAISEN x HAKKOI</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur veniam sapiente laboriosam maxime ad animi aut sed perferendis enim quo?</p>
+            <a href=""><button>Cek Selangkapnya</button></a>
+            </a>
+          </div>
+      </div>
+    </div>
+
     </>
   );
 };
--- src/componets/elements/NavBar.jsx
+++ src/componets/elements/NavBar.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart, faUser, faLanguage, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart, faUser, faLanguage, faBars} from "@fortawesome/free-solid-svg-icons";
 
 import Logo from "../../../public/asset/image/Logo.png";
+import Intagram from "../../../public/asset/image/icon/Instagram.png";
+import Facebook from "../../../public/asset/image/icon/Facebook.png";
+
 import "../../css/Navbar.css";
 import "../../css/index.css";
 
@@ -21,63 +24,87 @@ const Navbar = () => {
   const MenuLanguage = ["Indonesia", "English", "Japan"];
 
   return (
-    <nav>
-      <img src={Logo} alt="Logo" />
+    <>
+      <nav>
+        <img src={Logo} alt="Logo" />
 
-      <div className="Listbar-nav-dekstop">
-        <ul>
-          <li>Women</li>
-          <li>Men</li>
-          <li>Kids</li>
-          <li>Baby</li>
-        </ul>
-      </div>
+        <div className="Listbar-nav-dekstop">
+          <ul>
+            <li>Pakaian</li>
+            <li>Peralatan</li>
+            <li>Eletronik</li>
+            <li>AniManga</li>
+          </ul>
+        </div>
 
-      <div className="Listbar-nav">
-        <ul className={menuOpen ? "active" : ""}>
-          <div className="Lisbar-nav-hr">
-            <li>Women</li>
-            <hr />
-          </div>
-          <div className="Lisbar-nav-hr">
-            <li>Men</li>
-            <hr />
-          </div>
-          <div className="Lisbar-nav-hr">
-            <li>Kids</li>
-            <hr />
-          </div>
-          <div className="Lisbar-nav-hr">
-            <li>Baby</li>
-            <hr />
-          </div>
-        </ul>
-      </div>
+        <div className="Listbar-nav">
+          <ul className={menuOpen ? "active" : ""}>
+            <div className="Lisbar-nav-hr">
+              <li>Women</li>
+              <hr />
+            </div>
+            <div className="Lisbar-nav-hr">
+              <li>Men</li>
+              <hr />
+            </div>
+            <div className="Lisbar-nav-hr">
+              <li>Kids</li>
+              <hr />
+            </div>
+            <div className="Lisbar-nav-hr">
+              <li>Baby</li>
+              <hr />
+            </div>
+          </ul>
+        </div>
 
-      <div className="navbar-icons">
-        <button className="icon-btn menu-bars" onClick={toggleMenu}>
-          <FontAwesomeIcon icon={faBars} />
-        </button>
-        <button className="icon-btn menu-normal">
-          <FontAwesomeIcon icon={faShoppingCart} />
-        </button>
-        <button className="icon-btn menu-normal">
-          <FontAwesomeIcon icon={faUser} />
-        </button>
-        <div className="language-dropdown">
-          <button className="icon-btn menu-normal" onClick={toggleLanguageDropdown}>
-            <FontAwesomeIcon icon={faLanguage} />
+        <div className="navbar-icons">
+          <button className="icon-btn menu-bars" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} />
           </button>
-          {languageOpen && (
-            <ul className="dropdown-menu">
-              {MenuLanguage.map((menu) => (
-                <li key={menu}>{menu}</li>
-              ))}
-            </ul>
-          )}
+          <button className="icon-btn menu-normal">
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </button>
+          <div className="language-dropdown">
+            <button className="icon-btn menu-normal" onClick={toggleLanguageDropdown}>
+              <FontAwesomeIcon icon={faLanguage} />
+            </button>
+            {languageOpen && (
+              <ul className="dropdown-menu">
+                {MenuLanguage.map((menu) => (
+                  <li key={menu}>{menu}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+          <button className="botton-login menu-normal">
+            Login
+          </button>
+        </div>
+      </nav>
+
+      {/* Navbar Second */}
+      <div className="navSecond">
+        <ul className="navSecond-Left">
+          <span className="dot"></span>
+          <li>Spesial</li>
+          <span className="dot"></span>
+          <li>Kolaborasi</li>
+          <span className="dot"></span>
+          <li>Unggulan</li>
+          <span className="dot"></span>
+          <li>Limited Edition</li>
+          <li><span className="diskon">Diskon</span></li>
+        </ul>
+        <div className="navSecond-Right">
+          <ul>
+            <li>Sosial Media:</li>
+            <a href=""><img height="19px" src={Intagram} alt="Instagram" /></a>
+            <a href=""><img height="19px" src={Facebook} alt="Facebook" s/></a>
+          </ul>
         </div>
       </div>
-    </nav>
+    </>
   );
 };
 
